e921a99272f7669b7aec5a579c25be7cc2badd94
atria10 <mario.atria@stevejobs.academy>
2022-04-05 15:50:16 +0200

    feat(login-page.ts personal.page.ts tab5.page.ts) added personal page and implemented the logic at the start og the app based on wheater or not the user is logged in

---
+++ ionic-web-app/src/app/pages/personal/personal.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { PrimeService } from 'src/app/services/prime.service';
+
+@Component({
+  selector: 'app-personal',
+  templateUrl: './personal.page.html',
+  styleUrls: ['./personal.page.scss'],
+})
+export class PersonalPage implements OnInit {
+
+  user: User = {} as User
+
+  constructor(private service: PrimeService,private router: Router) { }
+
+  async ngOnInit() {
+    this.user = await this.service.personalInfo().then(response => response.user)
+  }
+
+  logout(){
+    this.service.logout();
+    this.router.navigate(['/signin'])
+  }
+}
