0ad2e63195a9a85b919b5d2d29d29baae0105261
atria10 <mario.atria@stevejobs.academy>
2022-04-05 10:31:13 +0200

    feat(prime.service.ts) Service completed

--- ionic-web-app/src/app/services/prime.service.ts
+++ ionic-web-app/src/app/services/prime.service.ts
@@ -6,6 +6,7 @@ import { HttpParams } from "@angular/common/http";
 import { Movie } from '../models/movies.model';
 import { ResponseMe, UserSignin, UserSignup } from '../models/user.model';
 import { SigninResponse } from '../models/token.models';
+import { Favorites } from '../models/favorites.model';
 const { primeEndpoint } = environment;
 
 @Injectable({
@@ -13,28 +14,84 @@ const { primeEndpoint } = environment;
 })
 export class PrimeService {
   constructor(private httpClient: HttpClient, private router: Router) { }
-  accessToken:string;
+  accessToken: string;
   setAccessToken(token: string) {
     this.accessToken = token;
   }
+  //MOVIES-------
   allMovies() {
     return this.httpClient.get(`${primeEndpoint}movies`).toPromise() as Promise<Movie[]>;
   }
-  specificMovie(id:number) {
+  specificMovie(id: number) {
     return this.httpClient.get(`${primeEndpoint}movies/${id}`);
   }
-  signup(user: UserSignup) {
-    return this.httpClient.post(`${primeEndpoint}auth/signup`, user,{observe: 'response'});
-  }
+  //--------MOVIES
 
+  //AUTH----------
+  signup(user: UserSignup) {
+    return this.httpClient.post(`${primeEndpoint}auth/signup`, user, { observe: 'response' });
+  }
   signin(user: UserSignin) {
-     return this.httpClient.post(`${primeEndpoint}auth/signin`, user,{observe: 'response'});
-    }
-  personalInfo(){
-    var reqHeader = new HttpHeaders({ 
+    return this.httpClient.post(`${primeEndpoint}auth/signin`, user, { observe: 'response' });
+  }
+  
+  personalInfo() {
+    var reqHeader = new HttpHeaders({
       'Content-Type': 'application/json',
       'Token': 'Bearer ' + this.accessToken
-   });
-    return this.httpClient.get(`${primeEndpoint}/auth/me`,{headers:reqHeader}).toPromise() as Promise<ResponseMe>;
+    });
+    return this.httpClient.get(`${primeEndpoint}/auth/me`, { headers: reqHeader }).toPromise() as Promise<ResponseMe>;
   }
+  //-----------AUTH
+
+  //USERS-----------
+  getFavorites() {
+    var reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Token': 'Bearer ' + this.accessToken
+    });
+    return this.httpClient.get(`${primeEndpoint}users/favorites`, { headers: reqHeader }).toPromise() as Promise<Favorites>;
+  }
+  addFavorite(movie: Movie) {
+    var reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Token': 'Bearer ' + this.accessToken
+    });
+    return this.httpClient.post(`${primeEndpoint}users/favorites`, movie, { observe: 'response', headers: reqHeader });
+  }
+  removeFavorite(userID: string, movieID: string) {
+    var reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Token': 'Bearer ' + this.accessToken
+    });
+    return this.httpClient.delete(`${primeEndpoint}users/${userID}/favorites/${movieID}`, { observe: 'response', headers: reqHeader });
+
+  }
+  //-----------USERS
+
+  //ADMINS-----------
+  addMovie(movie: Movie) {
+    var reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Token': 'Bearer ' + this.accessToken
+    });
+    return this.httpClient.post(`${primeEndpoint}admins/movies`, movie, { observe: 'response', headers: reqHeader });
+  }
+  removeMovie(movieID: string) {
+    var reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Token': 'Bearer ' + this.accessToken
+    });
+    return this.httpClient.delete(`${primeEndpoint}admins/movies/${movieID}`, { observe: 'response', headers: reqHeader });
+
+  }
+  addAdmin(username: string){
+    var reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Token': 'Bearer ' + this.accessToken
+    });
+    return this.httpClient.put(`${primeEndpoint}admins/user/${username}`,{ observe: 'response', headers: reqHeader });
+  }
+  //-----------ADMINS
+
 }
